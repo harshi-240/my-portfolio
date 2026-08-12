be13b5e0b39922f9a26b5222d612b45ba98f9070
harshi-240 <ptdrharshi6@gmail.com>
2026-08-12 18:05:54 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -549,3 +549,21 @@ contactForm.addEventListener("submit", function(e) {
     });
 
 });
+
+
+// Mobile Menu Toggle
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
+
+// Close menu after clicking a navigation link
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', () => {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    });
+});
